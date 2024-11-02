4dd7173544ed9d7d37daf64ba12f4db65d217b98
tinrooster <ach@tinrooster.com>
2024-11-02 05:15:00 -0700

    netlify checks, not getting newest rev2?

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true
-  },
-  trailingSlash: true,
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
 module.exports = nextConfig 
